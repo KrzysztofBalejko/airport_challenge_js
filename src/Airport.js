b970eee95d42d0181ea2208aca40dd025b004dec
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-02-05 17:38:23 +0000

    Added condition to clearForTakeOff

--- src/Airport.js
+++ src/Airport.js
@@ -11,5 +11,12 @@ Airport.prototype.clearForLanding = function(plane){
 };
 
 Airport.prototype.clearForTakeOff = function(){
+  if (this.isStormy()) {
+    throw new Error('cannot takeoff during storm');
+  }
   this._hangar.pop();
+};
+
+Airport.prototype.isStormy = function(){
+  return false;
 };
