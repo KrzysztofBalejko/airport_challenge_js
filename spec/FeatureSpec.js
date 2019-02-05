e04ae22421972de59afef854121d35c24f8bcd3f
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-02-05 08:19:51 +0000

    Test: plane can land at the airport

---
+++ spec/FeatureSpec.js
@@ -0,0 +1,16 @@
+describe('Feature test:', function(){
+
+  var plane;
+  var airport;
+
+  beforeEach(function(){
+    plane = new Plane();
+    airport = new Airport();
+  })
+
+  it('plane can be instructed to land at the airport',function(){
+    plane.land(airport);
+    expect(airport.planes()).toContain(plane);
+  });
+
+});
