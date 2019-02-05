8b199bc45173663488ab3753848a3a24e6a8a946
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-02-05 17:37:44 +0000

    Tests: checking for stormy condition. Under stormy does not clear planes for takeoff

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -23,4 +23,16 @@ describe('Airport', function(){
     expect(airport.planes()).toEqual([]);
   });
 
+  it('can check for stormy conditions', function(){
+    expect(airport.isStormy()).toBeFalsy();
+  });
+
+  describe('under stormy conditions', function(){
+    it('does not clear planes for takeoff', function(){
+      spyOn(airport,'isStormy').and.returnValue(true);
+      expect(function(){ airport.clearForTakeOff(); }).toThrowError('cannot takeoff during storm');
+    });
+  });
+
+
 });
